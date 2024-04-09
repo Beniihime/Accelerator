0b84c2d27b632ef337574c70c30d3dd66b6be7af
Beniihime <carpul8089@gmail.com>
2024-04-09 11:05:28 +0600

    setting

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import svgLoader from 'vite-svg-loader'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    VueDevTools(),
+    svgLoader(),
   ],
   server: {
     host: "0.0.0.0"
